f76b91e8465e67dfe22b1f0e3c58820966fdf718
Lucas Léo <lucasleoviegas@gmail.com>
2026-05-14 18:57:24 +0200

    Add command line argument support

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
+import argparse
+
 from app.organizer import organize_folder
 from app.utils import setup_logging
+
+def get_arguments():
+    parser = argparse.ArgumentParser(
+        description="Organize files in a folder by extension."
+        )
+
+    parser.add_argument(
+        "folder_path",
+        nargs="?",
+        help="Path to the folder that will be organized."
+        )
+    return parser.parse_args()
 
 def main() -> None:
     setup_logging()
     
     print("\nFile organizer\n")
-    path = input("Inform the path to the folder you wish to organize: ")
+
+    args = get_arguments()
+
+    if args.folder_path:
+        path = args.folder_path
+    else:
+        path = input("Inform the path to the folder you wish to organize: ")
+
     organize_folder(path)
 
 if __name__ == "__main__":
